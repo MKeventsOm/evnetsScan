210637e8dfa1e0340967397b34474af2ff75ac52
unknown <mk.events.om@gmail.com>
2026-04-06 01:19:31 +0400

    deploy

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+    
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'inkflow-secret-2024'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*")
 @socketio.on('start_session')
 def handle_start():
     # نرسل إشارة لجميع صفحات العرض المفتوحة
@@ -31,10 +34,10 @@ def handle_clear():
 @socketio.on('end_session')
 def handle_end():
     emit('end_session', broadcast=True, include_self=False)
+    
+
+   
 if __name__ == '__main__':
-    print("\n" + "="*45)
-    print("  ✓ InkFlow يعمل!")
-    print("  لوحة الكتابة  : http://127.0.0.1:5000")
-    print("  العرض المباشر : http://127.0.0.1:5000/view")
-    print("="*45 + "\n")
-    socketio.run(app, debug=False, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))
+    # Use socketio.run for better WebSocket support
+    socketio.run(app, host='0.0.0.0', port=port)
